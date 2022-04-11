5f1cdbf0cfd32c1d39806755f176c08c35cbfe72
Igor <98356121+writingor@users.noreply.github.com>
2022-04-11 16:10:02 +0200

    Create postList and js db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
-import KeyWordCheckbox from './components/UI/check-box/KeyWordCheckbox';
-import React, { useState } from 'react';
-import MainButton from './components/UI/button/MainButton';
-import PostItem from './components/post/PostItem';
+import React from 'react';
 import Header from './components/header/Header';
-import PostModal from './components/UI/modal/PostModal';
-import PostMenuButton from './components/UI/button/PostMenuButton';
+import PostsList from './components/posts_list/PostsList';
 
 function App() {
-
-  const [checked, setChecked] = useState(false);
-  const [postModalActive, setPostModalActive] = useState(false);
-
-  const toggleChecked = () => {
-    setChecked(!checked);
-  };
-
-  const doNothing = (e) => {
-    e.preventDefault();
-  }
-
-
 
   return (
     <div className="App">
@@ -30,50 +13,14 @@ function App() {
       <div className="wrapper">
 
         <main className='content'>
-          {/* test Post START */}
-          <PostItem onClick={doNothing}>
-            <PostMenuButton active={postModalActive} setActive={setPostModalActive} />
-          </PostItem>
 
-          <PostItem onClick={doNothing}>
-            <PostMenuButton active={postModalActive} setActive={setPostModalActive} />
-          </PostItem>
-          {/* test Post END */}
+          {/* test postlist START*/}
+          <PostsList></PostsList>
+          {/* test postlist END */}
         </main>
 
-        {/* test modal START */}
 
-
-        <PostModal active={postModalActive} setActive={setPostModalActive} >
-          {/* children here */}
-          {/* test checkbox START */}
-          <KeyWordCheckbox
-            isNameIdFor="react"
-            value={checked}
-            onChange={toggleChecked}
-          >
-            React
-          </KeyWordCheckbox>
-
-          <KeyWordCheckbox
-            isNameIdFor="c-sharp"
-            value={checked}
-            onChange={toggleChecked}
-          >
-            C#
-          </KeyWordCheckbox>
-
-          <KeyWordCheckbox
-            isNameIdFor="js"
-            value={checked}
-            onChange={toggleChecked}
-          >
-            JavaScript
-          </KeyWordCheckbox>
-          {/* test checkbox END */}
-          <MainButton>Найти похожие</MainButton>
-        </PostModal>
-        {/* test modal END */}
+       
 
       </div>
 
--- src/components/post/PostItem.jsx
+++ src/components/post/PostItem.jsx
@@ -3,39 +3,24 @@ import React from 'react';
 import css from './PostItem.module.css';
 import exampleImage from '../../assets/img/cat1.jpg';
 
-const PostItem = ({ children, image, ...props }) => {
+const PostItem = ({ postItem, children, image, ...props }) => {
 
     return (
         <div {...props} className={css.PostItem}>
 
             {children}
 
-            <h2 className={css.PostItem__title}>HTTP - заголовки и т.д.</h2>
+            <h2 className={css.PostItem__title}>{postItem.title}</h2>
 
-            <p className={css.PostItem__text}>
-                Это заголовки. Бла-бла-бла. Солнце греет, но пятки мёрзнут. Что делать с этим
-                знают только камыши. Стоят ночами и днями они в тёплой грязи. Мне же ногу греет
-                мотор. Он закреплен под седлом, пьёт бензин... Бла-бла-бла-бла-бла
-                -бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла
-                -бла-бла-бла-бла-бла-бла...
-            </p>
+            <p className={css.PostItem__text}>{postItem.content.text}</p>
 
-            <div className={css.PostItem__imagebox}>
+            {/* <div className={css.PostItem__imagebox}>
                 <div className={css.PostItem__image}>
                     <img src={exampleImage} alt="cat" />
                 </div>
-            </div>
+            </div> */}
 
-            <p className={css.PostItem__text}>
-                Это заголовки. Бла-бла-бла. Солнце греет, но пятки мёрзнут. Что делать с этим
-                знают только камыши. Стоят ночами и днями они в тёплой грязи. Мне же ногу греет
-                мотор. Он закреплен под седлом, пьёт бензин...<br /><br />
-                Бла-бла-бла-бла-бла
-                -бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла
-                -бла-бла-бла-бла-бла-бла...
-            </p>
-
-            <div className={css.PostItem__codeblock}>
+            {/* <div className={css.PostItem__codeblock}>
                 <pre className={css.codeblock__code}>
 
                     &#123;<br />
@@ -43,44 +28,7 @@ const PostItem = ({ children, image, ...props }) => {
                     &#125;
 
                 </pre>
-            </div>
-
-            <div className={css.PostItem__imagebox}>
-                <div className={css.PostItem__image}>
-                    <img src={exampleImage} alt="cat" />
-                </div>
-                <div className={css.PostItem__image}>
-                    <img src={exampleImage} alt="cat" />
-                </div>
-                <div className={css.PostItem__image}>
-                    <img src={exampleImage} alt="cat" />
-                </div>
-                <div className={css.PostItem__image}>
-                    <img src={exampleImage} alt="cat" />
-                </div>
-                <div className={css.PostItem__image}>
-                    <img src={exampleImage} alt="cat" />
-                </div>
-            </div>
-
-            <div className={css.PostItem__codeblock}>
-                <pre className={css.codeblock__code}>
-
-                    &#123;<br />
-                    &nbsp;&nbsp;&nbsp;&nbsp;test code;<br />
-                    &#125;
-
-                </pre>
-            </div>
-
-            <p className={css.PostItem__text}>
-                Это заголовки. Бла-бла-бла. Солнце греет, но пятки мёрзнут. Что делать с этим
-                знают только камыши. Стоят ночами и днями они в тёплой грязи. Мне же ногу греет
-                мотор. Он закреплен под седлом, пьёт бензин... <br />
-                Бла-бла-бла-бла-бла
-                -бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла
-                -бла-бла-бла-бла-бла-бла...
-            </p>
+            </div> */}
 
         </div>
     );
